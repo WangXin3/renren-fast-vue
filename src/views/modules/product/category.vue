66864e9c1b68dcc48478fba8051cdd3ef08dfee0
wangxinxin <91907@163.com>
2020-07-16 14:32:03 +0800

    菜单删除和新增

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,5 +1,45 @@
 <template>
-  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <div>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      @node-click="handleNodeClick"
+      :expand-on-click-node="false"
+      show-checkbox
+      node-key="catId"
+      :default-expanded-keys="expandedKey"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            type="text"
+            v-if="node.level <= 2"
+            size="mini"
+            @click="() => append(data)"
+          >Append</el-button>
+          <el-button
+            type="text"
+            v-if="node.childNodes.length == 0"
+            size="mini"
+            @click="() => remove(node, data)"
+          >Delete</el-button>
+        </span>
+      </span>
+    </el-tree>
+
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCategory">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -16,6 +56,16 @@ export default {
       defaultProps: {
         children: "children",
         label: "name"
+      },
+      expandedKey: [],
+      dialogVisible: false,
+      category: {
+        name: "",
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 1,
+        sort: 0,
+        productCount: 0
       }
     };
   },
@@ -39,11 +89,71 @@ export default {
           this.menus = [];
         }
       });
+    },
+    append(data) {
+      this.dialogVisible = true;
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
+    },
+
+    remove(node, data) {
+      this.$confirm(`是否删除【${data.name}】菜单?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          let ids = [data.catId];
+          this.$http({
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids, false)
+          }).then(({ data }) => {
+            this.$message({
+              type: "success",
+              message: "删除成功!"
+            });
+            // 刷新菜单
+            this.getMenus();
+            // 设置需要默认展开的菜单
+            this.expandedKey = [node.parent.data.catId];
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
+
+    // 添加三级分类的方法
+    addCategory() {
+      console.log(this.category);
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false)
+      }).then(({ data }) => {
+        this.$message({
+          type: "success",
+          message: "新增成功!"
+        });
+
+        // 关闭对话框
+        this.dialogVisible = false;
+
+        // 刷新菜单
+        this.getMenus();
+
+        // 设置需要默认展开的菜单
+        this.expandedKey = [this.category.parentCid];
+      });
     }
   },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {
-    this.getMenus()
+    this.getMenus();
   },
   //生命周期 - 挂载完成（可以访问DOM元素）
   mounted() {},
